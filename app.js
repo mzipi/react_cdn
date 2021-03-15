10b3f39299355cfc9d055a42ba05bf8fc24cba2b
Michell Zipitría <mzipi@outlook.com>
2021-03-14 21:52:17 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+ReactDOM.render( React.createElement(React.StrictMode, null, React.createElement("div", {
+  class: "wrapper"
+}, React.createElement("div", {
+  class: "item1"
+}, "BIENVENIDO A REACT"), React.createElement("div", {
+  class: "item2"
+}, "EL SITIO ESTA EN CONSTRUCCION"))), document.getElementById('root'));
