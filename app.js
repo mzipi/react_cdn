6248603ae35fe5569505dde25afbf157391fb5d6
Michell Zipitría <mzipi@outlook.com>
2021-07-11 17:58:00 -0300

    main

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-ReactDOM.render( React.createElement(React.StrictMode, null, React.createElement("div", {
-  class: "wrapper"
-}, React.createElement("div", {
-  class: "item1"
-}, "BIENVENIDO A REACT"), React.createElement("div", {
-  class: "item2"
-}, "EL SITIO ESTA EN CONSTRUCCION"))), document.getElementById('root'));
+ReactDOM.render(
+  React.createElement(
+    React.StrictMode, null, React.createElement(
+      "div", {
+        class: "wrapper"
+      }, React.createElement(
+        "div", {
+          class: "item1"
+        }, "BIENVENIDO A REACT"
+      ), React.createElement(
+        "div", {
+          class: "item2"
+        }, "EL SITIO ESTA EN CONSTRUCCION"
+      )
+    )
+  ),
+  document.getElementById('root')
+);
